bae4ca124103f8046cdbb56ca5df245103b1534b
Blazing Byte <blazingbyte@MBs-MacBook-Pro.local>
2023-12-10 17:44:29 +0800

    Initial commit

---
+++ src/router/index.ts
@@ -0,0 +1,48 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import { useAppStore } from '@/stores/index';
+import appSetting from '@/app-setting';
+
+import HomeView from '../views/index.vue';
+
+const routes: RouteRecordRaw[] = [
+    // dashboard
+    { path: '/', name: 'home', component: HomeView },
+
+    // auth
+    {
+        path: '/login',
+        name: 'login',
+        component: () => import('../views/auth/login.vue'),
+        meta: { layout: 'auth' },
+    },
+
+    { path: '/register', name: 'register', component: () => import('../views/auth/register.vue'), meta: { layout: 'auth' } },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    linkExactActiveClass: 'active',
+    routes,
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition;
+        } else {
+            return { left: 0, top: 0 };
+        }
+    },
+});
+
+router.beforeEach((to, from, next) => {
+    const store = useAppStore();
+
+    if (to?.meta?.layout == 'auth') {
+        store.setMainLayout('auth');
+    } else {
+        store.setMainLayout('app');
+    }
+    next(true);
+});
+router.afterEach((to, from, next) => {
+    appSetting.changeAnimation();
+});
+export default router;
